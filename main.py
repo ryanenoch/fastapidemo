c9912c8f22b47a9caa636e6fe54bb77eae3f4c72
ryanenoch <105472843+ryanenoch@users.noreply.github.com>
2025-04-02 09:25:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from gas import gas
 
 app = FastAPI()
-
+price = gas()
 
 @app.get("/")
 async def root():
     return {"Message": "Congrats! This is your first API!"}
+    return price
